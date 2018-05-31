e734e0c27796fb70c625c9c5c5c84e0ca304c458
Sam Deane <sam@elegantchaos.com>
2018-05-31 16:52:34 +0100

    Require Swift 4.2.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,5 +24,6 @@ let package = Package(
         .testTarget(
             name: "BuilderConfigurationTests",
             dependencies: ["BuilderConfiguration"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v4_2]
 )
